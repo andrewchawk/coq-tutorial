c0d1d2bdf7fc2fe54893ef96605090895918a66e
onestruggler <bian@dal.ca>
2024-05-24 15:59:47 +0800

    add linebreaks

--- coq-tutorial.v
+++ coq-tutorial.v
@@ -22,30 +22,32 @@ https://www.cs.cornell.edu/courses/cs3110/2018sp/a5/coq-tactics-cheatsheet.html
 *)
 
 
-(* 
-First of all, let's download and install CoqIDE. You can run this file in it.
+(* First of all, let's download and install CoqIDE. You can run this
+file in it.
 
 For Windows and MacOS users, you can download from here,
 https://www.mathstat.dal.ca/~xbian/uploads/softwares/
 
-For Linux users, please run this command in your terminal:
-sudo snap install coq-prover
+For Linux users, please run this command in your terminal: sudo snap
+install coq-prover
 
-Or refer to here,
-https://snapcraft.io/coq-prover
+Or refer to here, https://snapcraft.io/coq-prover
 
 *)
 
 
-(*
-We start with data types, and along the way, we also pick up some Coq basics.
-For now, we just regard "type" as "set". Integers have type Int, and the conversely, the type Int is the set of all integers.
+(* We start with data types, and along the way, we also pick up some
+Coq basics.  For now, we just regard "type" as "set". Integers have
+type Int, and the conversely, the type Int is the set of all integers.
 *)
 
 (* Empty type or Empty set *) 
 Inductive Empty : Set := .
 
-(* In CoqIDE, press Ctrl + rightarrow to type-check things defined till the cursor, Ctrl + downarrow to type-check the next clause, Ctrl + uparrow to type-check the previous clause, and Ctrl + End to type-check the whole file. *)
+(* In CoqIDE, press Ctrl + rightarrow to type-check things defined
+till the cursor, Ctrl + downarrow to type-check the next clause, Ctrl
++ uparrow to type-check the previous clause, and Ctrl + End to
+type-check the whole file. *)
 
 (* Singleton type or Sinlgeton set *)
 Inductive Singleton : Set := singleton.
@@ -67,7 +69,7 @@ Definition f2 : Singleton -> Singleton := fun (s : Singleton) =>
     singleton => singleton
   end.
 
-(* Alternative definitions. We will show f2 = f2a, f2 = f2b, f2 = f2c later. *)
+(* Alternative definitions. *)
 
 Definition f2a (s : Singleton) : Singleton :=
   match s with
@@ -96,9 +98,8 @@ Definition f4 (s : Singleton) :=
   end.
 *)
 
-(*
-In fact, there is no fucntion to Empty from any non-empty set. f0 is the only possible function to Empty.
-*)
+(* In fact, there is no fucntion to Empty from any non-empty set. f0
+is the only possible function to Empty.  *)
 
 Inductive Bool : Set := true | false.
 
@@ -147,26 +148,32 @@ Notation "x + y" := (add x y) (at level 50, left associativity).
 Compute (zero + zero + zero).
 Compute (3 + 4).
 
-(* We want to show add is associative, i.e., forall (a b c), a + (b + c) = (a + b) + c. *)
+(* We want to show add is associative, i.e., forall (a b c), a + (b +
+c) = (a + b) + c. *)
 
-(* Note that, since we define + to be left assocative, (a + b) + c is exactly the same as a + b + c. *)
+(* Note that, since we define + to be left assocative, (a + b) + c is
+exactly the same as a + b + c. *)
 
 (* 
 what is "forall" and what is "="?
 In other words, how to express forall quantifier and equality in Coq?
  *)
 
-(* 
-The addition we defined is implicitly quantified by forall, since it says we can add any two natural number. In general, any function defined in Coq is total and hence forall quantified. This suggests that we should define associativity as a fucntion on a, b, c, which returns the "equal" property. E.g., something like this,
-*)
+(* The addition we defined is implicitly quantified by forall, since
+it says we can add any two natural number. In general, any function
+defined in Coq is total and hence forall quantified. This suggests
+that we should define associativity as a fucntion on a, b, c, which
+returns the "equal" property. E.g., something like this, *)
 
 (*
 Definition assoc : Nat -> Nat -> Nat -> Equality Property
 *)
 
-(*
-Instead, let use reserved word Theorem instead of Definition. In Coq, there is not much difference between the following keywords: Defintion; Lemma; Theorem; Example; Remark; Fact. The difference is mainly for documentation. Coq may treat them slightly differently, but not a lot.
-*)
+(* Instead, let use reserved word Theorem instead of Definition. In
+Coq, there is not much difference between the following keywords:
+Defintion; Lemma; Theorem; Example; Remark; Fact. The difference is
+mainly for documentation. Coq may treat them slightly differently, but
+not a lot.  *)
 
 (*
 Theorem assoc : Nat -> Nat -> Nat -> Equality Property
@@ -174,11 +181,10 @@ Theorem assoc : Nat -> Nat -> Nat -> Equality Property
 
 (* The main problem is how to express equality property. *)
 
-(* 
-First try, since there is two possibilty, either equal or not equal,
-maybe lets use Bool to indicate this. We can define a function tatking
-two natural numbers, and if they are equal, it return "true", otherwise "false". 
-*)
+(* First try, since there is two possibilty, either equal or not
+equal, maybe lets use Bool to indicate this. We can define a function
+tatking two natural numbers, and if they are equal, it return "true",
+otherwise "false".  *)
 
 Fixpoint equalNB (a : Nat) (b : Nat) : Bool := 
     match a with
@@ -195,12 +201,12 @@ Fixpoint equalNB (a : Nat) (b : Nat) : Bool :=
 
 (*
 Theorem assoc : Nat -> Nat -> Nat -> equalNB (a + (b + c)) (a + b + c) = true.
+
 Theorem assoc : Nat -> Nat -> Nat -> equalNB (a + (b + c)) (a + b + c) "always equals to" true.
 *)
 
-(*
-Again, we need equality for Bool. Like for Nat, we can define equalilty test for Bool, easier than for Nat.
-*)
+(* Again, we need equality for Bool. Like for Nat, we can define
+equalilty test for Bool, easier than for Nat.  *)
 
 Definition equalBB (a : Bool) (b : Bool) : Bool := 
     match a with
@@ -214,20 +220,48 @@ Definition equalBB (a : Bool) (b : Bool) : Bool :=
               end
       end.  
 
-(* 
-We saw some redundancy there, awkwardly. But still, what does it mean by "always equals to"? The equality test functions we defined don't guarantee "always equality" obviously, since they might return false. This method might be enough for testing in software engineering. We have a test function, we just run thousands of test cases to see if we always get true return. It is not a proof. Also we have infinite many inputs, the test can never be complete.
+(* We saw some redundancy there, awkwardly. But still, what does it
+mean by "always equals to"? The equality test functions we defined
+don't guarantee "always equality" obviously, since they might return
+false. This method might be enough for testing in software
+engineering. We have a test function, we just run thousands of test
+cases to see if we always get true return. It is not a proof. Also we
+have infinite many inputs, the test can never be complete.
 
-The magical leap happens here (comparing to ordinary programming languages).
+The magical leap happens here (comparing to ordinary programming
+languages).
 
-We know a fucntion "always" return a value when fed with an input. We might be able to use this feature to capture "always". We know for a function, there must be a domain and a codomain, in other words, an input type and an output type. Let the domain be (Nat x Nat x Nat), what is the codomain? A type about "equality". We need "equality type"!
+We know a fucntion "always" return a value when fed with an input. We
+might be able to use this feature to capture "always". We know for a
+function, there must be a domain and a codomain, in other words, an
+input type and an output type. Let the domain be (Nat x Nat x Nat),
+what is the codomain? A type about "equality". We need "equality
+type"!
 
-Given natural numbers a b, we want a = b to be a type! not a test whether a equals b, but a type. Not a value, but a set, a type. We want that there exits a function from Nat x Nat x Nat to lhs = rhs, whenever lhs is equal to the rhs, and we want that there is no fucntion from Nat x Nat x Nat to lhs = rhs if they are not equal.
+Given natural numbers a b, we want a = b to be a type! not a test
+whether a equals b, but a type. Not a value, but a set, a type. We
+want that there exits a function from Nat x Nat x Nat to lhs = rhs,
+whenever lhs is equal to the rhs, and we want that there is no
+fucntion from Nat x Nat x Nat to lhs = rhs if they are not equal.
 
-Recall, Empty is such that there is no function to it (excepty from Empty). So we define lhs = rhs to be Empty if lhs is not equal to rhs. I forgot to mention that there is always an unique function from any set to Singleton, which just map everything to singleton. Lets try to define lhs=rhs to be Singleton if lhs is equal to rhs.
+Recall, Empty is such that there is no function to it (excepty from
+Empty). So we define lhs = rhs to be Empty if lhs is not equal to
+rhs. I forgot to mention that there is always an unique function from
+any set to Singleton, which just map everything to singleton. Lets try
+to define lhs=rhs to be Singleton if lhs is equal to rhs.
 
-To sum up, lhs=rhs is either Empty or Singleton, depending on what values lhs and rhs are. In other word, lhs=rhs is a family of types (either Empty or Singleton) parametered by lhs and rhs, which are value. Such a pattern, type family indexed by values of another type, is call "dependent type". Both Coq and Agda support dependent types. Dependent types are essential in proof assistant, since we are interested in showing some value satisfty some property, where property is a set or a type.
+To sum up, lhs=rhs is either Empty or Singleton, depending on what
+values lhs and rhs are. In other word, lhs=rhs is a family of types
+(either Empty or Singleton) parametered by lhs and rhs, which are
+value. Such a pattern, type family indexed by values of another type,
+is call "dependent type". Both Coq and Agda support dependent
+types. Dependent types are essential in proof assistant, since we are
+interested in showing some value satisfty some property, where
+property is a set or a type.
 
-In summary, a function is powerful enough to express: 1) forall quantification; 2) "always" property, which will be expoited only if we have Equality type.
+In summary, a function is powerful enough to express: 1) forall
+quantification; 2) "always" property, which will be expoited only if
+we have Equality type.
 
 *)
 
@@ -242,16 +276,16 @@ Definition Equal' : Nat -> Nat -> Set :=
 Compute (Equal' 1 2).
 Compute (Equal' 3 3).
 
-(*
-We are still using the test function, but we should be smarter than that. We have the equivalent defintion without using the test fucntion. 
-*)
+(* We are still using the test function, but we should be smarter than
+that. We have the equivalent defintion without using the test
+fucntion.  *)
 
 Inductive Equal (a : Nat) : Nat -> Set := 
   | refl : Equal a a.
 
-(*
-This type reads: 1) there is only one element in Equal a a for each a in Nat, so like Singleton, but not exactly Singleton; 2) there is no elment in Equal a b provided a is not equal to b, which is Empty.
-*)
+(* This type reads: 1) there is only one element in Equal a a for each
+a in Nat, so like Singleton, but not exactly Singleton; 2) there is no
+elment in Equal a b provided a is not equal to b, which is Empty.  *)
 
 (* Let's make a Notation. Instead of = which is reserved, we use ==. *)
 
@@ -276,9 +310,8 @@ Theorem assoc (a : Nat) (b : Nat) (c : Nat) : a + (b + c) == a + b + c.
 
 Theorem assoc : forall (a : Nat) (b : Nat) (c : Nat), a + (b + c) == a + b + c.
 
-(* 
-A proof of this theorem is just a fucntion from Nat x Nat x Nat to the type a + (b + c) == a + (b + c). Let's contruct it.
-*)
+(* A proof of this theorem is just a fucntion from Nat x Nat x Nat to
+the type a + (b + c) == a + (b + c). Let's contruct it.  *)
 Proof. 
 intros a b c.
 induction a as [ | a' ih].
@@ -312,11 +345,16 @@ There are a lot to define:
   etc..
 *)
 
-(* 
-If we implement sorting in C, we will probably use array. In some other lauguage, it might be called Vector. One problem with that, vector or array always has a size or length, but we might have more things to sort than that length limitation.
+(* If we implement sorting in C, we will probably use array. In some
+other lauguage, it might be called Vector. One problem with that,
+vector or array always has a size or length, but we might have more
+things to sort than that length limitation.
 
-In Coq, or Agda, Haskell, or any other language that supports infinite long list, things are more elegant. We just sort on a list. Nota that the difference between vector and list is that vector has a fixed length (however large it is, it is finite), but list can be of any size. Let's define List.
-*)
+In Coq, or Agda, Haskell, or any other language that supports infinite
+long list, things are more elegant. We just sort on a list. Nota that
+the difference between vector and list is that vector has a fixed
+length (however large it is, it is finite), but list can be of any
+size. Let's define List.  *)
 
 Inductive ListNat : Set := 
   | nil : ListNat
@@ -332,9 +370,12 @@ Definition ObjectToSort := ListNat.
 
 (* This solves the first question --- what to sort? *)
 
-(*
-Apparently, we are sorting according which is larger and which is smaller, we want to sort them from small to large. We are facing the same problem when we define equality. We need more than just a testing function. We want something like 1 < 2 being a set, a type, especially a singleton set. Very similar to equality, we define LessOrEqual as follows:
-*)
+(* Apparently, we are sorting according which is larger and which is
+smaller, we want to sort them from small to large. We are facing the
+same problem when we define equality. We need more than just a testing
+function. We want something like 1 < 2 being a set, a type, especially
+a singleton set. Very similar to equality, we define LessOrEqual as
+follows: *)
 
 Inductive LessOrEqual : Nat -> Nat -> Set :=
   | LEzero : forall (b : Nat), LessOrEqual zero b
@@ -379,8 +420,9 @@ Definition not_succ_LE_zero (x : Nat) (p : succ x =< 0) : Empty :=
 match p with
 end.
 
-(*
-Transitivity of LessOrEqual relation. This definition is quite covluted. The main techniuque use here is "dependent pattern matching". See explainations here
+(* Transitivity of LessOrEqual relation. This definition is quite
+covluted. The main techniuque use here is "dependent pattern
+matching". See explainations here
 https://stackoverflow.com/questions/12544469/impossible-pattern-in-writing-implicit-proof-object-in-coq
 *)
 
@@ -404,19 +446,29 @@ intros a b H.
 apply (LE_trans a (succ a) b (x_le_xplus1 a) H).
 Qed.
 
-(* 
-Side: how to define negation in Coq. 
+(* Side: how to define negation in Coq.
 
-Recall in boolean logic, given a boolean variable b, (b => false) = true iff b = false, and (b => false) = false iff b = true. In other word, not b = b => false.
+Recall in boolean logic, given a boolean variable b, (b => false) =
+true iff b = false, and (b => false) = false iff b = true. In other
+word, not b = b => false.
 
-If A is a Set, then (A -> Empty) is the set of functions from A to Empty. A -> Empty is empty iff A is not Empty, and A -> Empty is not empty iff A is Empty. We will define Not A to be A -> Empty. 
+If A is a Set, then (A -> Empty) is the set of functions from A to
+Empty. A -> Empty is empty iff A is not Empty, and A -> Empty is not
+empty iff A is Empty. We will define Not A to be A -> Empty.
 
-The idea behind this is what is called the "Brouwer-Heyting-Kolmogorov intepretation" or "BHK interpretation" of intuitionistic logic, or "Curry-Howard correspondence". Roughly speaking, we can regard a set or a type as a proposition and elments as proofs. E.g., a == a is a type and a proposition that a is equal to a, and refl is an element of a == a, and also a proof.
+The idea behind this is what is called the "Brouwer-Heyting-Kolmogorov
+intepretation" or "BHK interpretation" of intuitionistic logic, or
+"Curry-Howard correspondence". Roughly speaking, we can regard a set
+or a type as a proposition and elments as proofs. E.g., a == a is a
+type and a proposition that a is equal to a, and refl is an element of
+a == a, and also a proof.
 
-Back to the negation of a set A. If A is empty, or has no proof, then Not A should have a proof, i.e., there is one function in A -> Empty. On the other hand, if A has a proof, then Not A shouldn't have any proof, i.e. A -> Empty is empty.
+Back to the negation of a set A. If A is empty, or has no proof, then
+Not A should have a proof, i.e., there is one function in A ->
+Empty. On the other hand, if A has a proof, then Not A shouldn't have
+any proof, i.e. A -> Empty is empty.
 
-To code the above, we have,
-*)
+To code the above, we have, *)
 
 Definition Not (A : Set) : Set := A -> Empty.
 
@@ -431,9 +483,9 @@ induction x.
   - exact (IHx (Less_succ_inv x x H)).
 Qed.
 
-(*
-This is also quite convoluted. Also using "dependent pattern matching" technique. 
-*)
+(* This is also quite convoluted. Also using "dependent pattern
+matching" technique.  *)
+
 Fixpoint LE_antisym (x y : Nat) (xy : x =< y) (yx : y =< x) : x == y :=
 match x as x1 return (forall (y : Nat), x1 =< y -> y =< x1 -> x1 == y) with
   | zero => fun y xy yx => match y as y1 return (zero =< y1 -> y1 =< zero -> zero == y1) with
@@ -456,7 +508,11 @@ Inductive OR (A B : Set) : Set :=
 Notation "A \_/ B" := (OR A B) (at level 85, right associativity).
 
 Check or_introl.
-(* Check or_introl show the type of or_introl "forall A B : Set, A -> A \_/ B". If calling or_introl "or_introl A B a", A can be infered from a. If from context, B is also inferable, we use the following command to tell Coq, we will omit type arguments A and B as often as possible. *)
+(* Check or_introl show the type of or_introl "forall A B : Set, A ->
+A \_/ B". If calling or_introl "or_introl A B a", A can be infered
+from a. If from context, B is also inferable, we use the following
+command to tell Coq, we will omit type arguments A and B as often as
+possible. *)
 Arguments or_introl [A B] _, [A] B _.
 Arguments or_intror [A B] _, A [B] _.
 
@@ -475,7 +531,8 @@ Fixpoint decide_order (a : Nat) (b : Nat) : (a =< b) \_/ (b <' a) :=
       end
   end.
 
-(* Have defined order on Nat and all kinds of properties of order. We are ready to define insertion sort. *)
+(* Have defined order on Nat and all kinds of properties of order. We
+are ready to define insertion sort. *)
 
 Fixpoint insert (x : Nat) (l : ListNat) : ListNat :=
   match l with 
@@ -524,7 +581,10 @@ match H with
   | sort_more a b l' ab bl' => bl'
 end.
 
-(* We also want the sorted list to be a permutation of the orginial list, or equivalently, we want two lists have the same elements (counting duplicates).*)
+(* We also want the sorted list to be a permutation of the orginial
+list, or equivalently, we want two lists have the same elements
+(counting duplicates).*)
+
 Fixpoint count_occ (l : ListNat) (x : Nat) : Nat :=
   match l with
     | nil => 0
